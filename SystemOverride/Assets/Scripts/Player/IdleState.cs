74ea693d899c984ab618ec85cdaf28d5b2c81d71
SHarkkk <54424781+SHark-Uni@users.noreply.github.com>
2025-12-11 16:53:53 +0900

    ### New InputSystem/ Player사진 업데이트

---
+++ SystemOverride/Assets/Scripts/Player/IdleState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : PlayerSuperState
+{
+	public IdleState(Player_Temp owner, StateMachine<Player_Temp> stateMachine, string name, Rigidbody2D rb, Animator am) 
+		: base(owner, stateMachine, name, rb, am)
+	{
+	}
+
+	public override void Enter()
+	{
+		base.Enter();
+	}
+
+	public override void EntityUpdate()
+	{
+		base.EntityUpdate();
+
+		//키입력을 한다면, Walk 상태로 전파
+		
+	}
+
+	public override void Exit()
+	{
+		base.Exit();
+	}
+}
