f7c5bffc435af6d207361931fd74413a4b103db7
hyungjin <91019153+hyungjin-yu@users.noreply.github.com>
2025-12-22 16:34:43 +0900

    튜토리얼맵 제작
    튜토리얼맵 1차 완성입니다

---
+++ SystemOverride/Assets/Scripts/First_Map/Box_Crush.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_Crush : MonoBehaviour
+{
+    // 플레이어가 쏜 탄환에 맞으면 박스가 부서지는 스크립트
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
