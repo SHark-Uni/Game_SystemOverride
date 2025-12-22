725e39d378efec56eb1bfb4493600f974394970e
SHarkkk <wnsrl4067@gmail.com>
2025-12-22 13:31:05 +0900

    ### 중복되는 파일 삭제

--- SystemOverride/Assets/SkillBarUI.cs
+++ SystemOverride/Assets/SkillBarUI.cs
@@ -5,16 +5,17 @@ using UnityEngine.UI;
 
 public class SkillBarUI : MonoBehaviour
 {
-    [SerializeField] private PlayerSkillController player;
+    //[SerializeField] private PlayerSkillController player;
     [SerializeField] private Button[] buttons = new Button[4];
 
     private void Awake()
     {
+        /*
         for (int i = 0; i < buttons.Length; i++)
         {
             int idx = i;
             if (buttons[idx] != null)
                 buttons[idx].onClick.AddListener(() => player.UseSkill(idx));
-        }
+        }*/
     }
 }
